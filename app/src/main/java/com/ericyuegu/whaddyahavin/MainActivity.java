814455cfed8ce5ee3574db18344a4a4ec0b8826b
coreycaskey <ccaskey6@gatech.edu>
2018-09-05 18:24:06 -0400

    Added email verification

--- app/src/main/java/com/ericyuegu/whaddyahavin/MainActivity.java
+++ app/src/main/java/com/ericyuegu/whaddyahavin/MainActivity.java
@@ -1,10 +1,14 @@
 package com.ericyuegu.whaddyahavin;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,7 +17,6 @@ import android.widget.Toast;
 
 import com.ericyuegu.whaddyahavin.auth.LoginActivity;
 import com.ericyuegu.whaddyahavin.auth.RegisterActivity;
-import com.ericyuegu.whaddyahavin.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -85,6 +88,34 @@ public class MainActivity extends AppCompatActivity {
         remove.setVisibility(View.GONE);
 
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        if (!user.isEmailVerified()) {
+            AlertDialog alert = new AlertDialog.Builder(MainActivity.this).create();
+            alert.setTitle("Verify Email");
+            alert.setMessage("Please verify account! Link sent to your email.");
+            alert.setButton(Dialog.BUTTON_POSITIVE,"OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    // Write your code here to execute after dialog closed
+                    user.sendEmailVerification().addOnCompleteListener(MainActivity.this, new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(MainActivity.this,
+                                        "Verification email sent to " + user.getEmail(),
+                                        Toast.LENGTH_SHORT).show();
+                            } else {
+                                Log.e("MainActivity", "sendEmailVerification", task.getException());
+                                Toast.makeText(MainActivity.this,
+                                        "Failed to send verification email. Ensure email is valid.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            });
+            alert.show();
+            alert.getButton(Dialog.BUTTON_POSITIVE).setTextColor(0xFF0097A7);
+        }
 
         if (progressBar != null) {
             progressBar.setVisibility(View.GONE);
