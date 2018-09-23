62a90d257caaaf7ce216b446801b3748640fe037
Jayden <jayden_sun@outlook.com>
2018-09-23 01:40:24 -0400

    Fixed send image to view meal Activty

--- app/src/main/java/com/ericyuegu/whaddyahavin/ViewMealActivity.java
+++ app/src/main/java/com/ericyuegu/whaddyahavin/ViewMealActivity.java
@@ -1,10 +1,10 @@
 package com.ericyuegu.whaddyahavin;
 
-import android.net.Uri;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.ImageView;
 
 public class ViewMealActivity extends AppCompatActivity {
@@ -14,13 +14,36 @@ public class ViewMealActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_meal);
 
-        String photoUri = getIntent().getExtras().getString("photo_uri");
-        if (photoUri != null)
-        {
-            Uri path = Uri.parse(photoUri);
-            ImageView img = (ImageView) findViewById(R.id.meal_image);
-            img.setImageURI(path);
-        }
+//        String newString;
+//        if (savedInstanceState == null) {
+//            Bundle extras = getIntent().getExtras();
+//            if(extras == null) {
+//                newString= null;
+//            } else {
+//                newString= extras.get("photo_uri");
+//            }
+//        } else {
+//            newString= (String) savedInstanceState.getSerializable("photo_uri");
+//        }
+
+        // System.out.println("newString: " + newString);
+
+        Bundle extras = getIntent().getExtras();
+        byte[] byteArray = extras.getByteArray("photo_uri");
+
+        Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+        ImageView image = findViewById(R.id.meal_image);
+
+        image.setImageBitmap(bmp);
+
+//        String photoUri = getIntent().getExtras().getString("photo_uri");
+//        System.out.println();
+//        if (photoUri != null)
+//        {
+//            Uri path = Uri.parse(photoUri);
+//            ImageView img = (ImageView) findViewById(R.id.meal_image);
+//            img.setImageURI(path);
+//        }
 
     }
 }
